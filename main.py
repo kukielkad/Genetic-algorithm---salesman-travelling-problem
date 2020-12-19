58a4187f34b13452c9539eebd8568a479cfe4ec0
kukielkad <62215988+kukielkad@users.noreply.github.com>
2020-12-19 23:38:14 +0100

    More data (added position)

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import pandas as pd
+import matplotlib as plt
 number_of_cars = 5
 car_capacity = 1000
 number_of_customers = 30
-main_dict = {"Białystok":500,"Bielsko_Biała":50,"Chrzanów":400,"Gdańsk":200,"Gdynia":100,"Gliwice":40,
-"Gromnik":200,"Katowice":300,"Kielce":30,"Krosno":60,"Krynica":50,"Lublin":60,"Łódź":160,"Malbork":100,"Nowy_Targ":120,
-"Olsztyn":300,"Poznań":100,"Puławy":200,"Radom":100,"Rzeszów":60,"Sandomierz":200,
-"Szczecin":150,"Szczucin":60,"Szklarska_Poręba":50,"Tarnów":70,"Warszawa":200,
-"Wieliczka":90,"Wrocław":40,"Zakopane":200,"Zamość":300}
-print(main_dict)
+main_dict = {"Bialystok":500,"Bielsko_Biala":50,"Chrzanow":400,"Gdansk":200,"Gdynia":100,"Gliwice":40,
+"Gromnik":200,"Katowice":300,"Kielce":30,"Krosno":60,"Krynica":50,"Lublin":60,"Lodz":160,"Malbork":100,"Nowy_Targ":120,
+"Olsztyn":300,"Poznan":100,"Pulawy":200,"Radom":100,"Rzeszow":60,"Sandomierz":200,
+"Szczecin":150,"Szczucin":60,"Szklarska_Poreba":50,"Tarnow":70,"Warszawa":200,
+"Wieliczka":90,"Wroclaw":40,"Zakopane":200,"Zamosc":300}
+position = pd.read_csv('data.csv',sep=';',encoding= 'unicode_escape')
+position
