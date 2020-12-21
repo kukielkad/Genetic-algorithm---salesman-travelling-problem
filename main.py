cfee39ad78167f9899c2ab4fed15cf1d1fb59c70
kukielkad <62215988+kukielkad@users.noreply.github.com>
2020-12-21 15:43:41 +0100

    distance matrix prepared

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import numpy as np
 import pandas as pd
 import matplotlib as plt
+from scipy.spatial.distance import squareform, pdist
+from sklearn.neighbors import DistanceMetric
 number_of_cars = 5
 car_capacity = 1000
 number_of_customers = 30
@@ -9,4 +12,11 @@ main_dict = {"Bialystok":500,"Bielsko_Biala":50,"Chrzanow":400,"Gdansk":200,"Gdy
 "Szczecin":150,"Szczucin":60,"Szklarska_Poreba":50,"Tarnow":70,"Warszawa":200,
 "Wieliczka":90,"Wroclaw":40,"Zakopane":200,"Zamosc":300}
 position = pd.read_csv('data.csv',sep=';',encoding= 'unicode_escape')
-position
+#choose distance metrics
+dist = DistanceMetric.get_metric('haversine')
+#convert degrees to radian
+position['latitude']=np.radians(position['latitude'])
+position['longitude']=np.radians(position['longitude'])
+#make distance matrix
+distance_matrix = dist.pairwise(position[['lat','lon']].to_numpy())*6373
+print(distance_matrix)
